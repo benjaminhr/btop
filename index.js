f8a20774b88251cfe0e78ca88d75dfd4285786e1
benjaminr177 <unoceptik@gmail.com>
2017-11-15 12:41:24 +0200

    Standard JS testing

--- index.js
+++ index.js
@@ -1,79 +1,78 @@
-const si = require('systeminformation');
-const temp = require('osx-temperature-sensor'); // only for mac
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+const si = require('systeminformation')
+const temp = require('osx-temperature-sensor') // only for mac
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
 
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 app.use(express.static('public'))
 
-app.get('/', (req,res) => {
-    res.sendFile('index.html')
+app.get('/', (req, res) => {
+  res.sendFile('index.html')
 })
 
-app.get('/temp', (req,res) => {
-    si.cpuTemperature().then(data => {
-        var temp = JSON.parse(data.main);
-        res.json({ temperature: temp})
-    })
+app.get('/temp', (req, res) => {
+  si.cpuTemperature().then(data => {
+    var temp = JSON.parse(data.main)
+    res.json({ temperature: temp})
+  })
 })
 
 app.get('/ps', (req, res) => {
-    si.processes().then(data => {
-        var ps = [];
-        data.list.forEach((item) => {
-            ps.push(item);
-        })
-
-        ps.sort((a, b) => {
-            return b.pcpu - a.pcpu;
-        })
-
-        ps.splice(10, ps.length-1); 
-        res.json(ps); 
+  si.processes().then(data => {
+    var ps = []
+    data.list.forEach((item) => {
+      ps.push(item)
     })
+
+    ps.sort((a, b) => {
+      return b.pcpu - a.pcpu
+    })
+
+    ps.splice(10, ps.length - 1)
+    res.json(ps)
+  })
 })
 
-app.get('/net', (req,res) => {
-    si.networkStats().then(data => {
-        res.json(data);
-    })
+app.get('/net', (req, res) => {
+  si.networkStats().then(data => {
+    res.json(data)
+  })
 })
 
-app.get('/os', (req,res) => {
-    si.osInfo().then(data => {
-        res.json(data);
-    })
+app.get('/os', (req, res) => {
+  si.osInfo().then(data => {
+    res.json(data)
+  })
 })
 
 app.get('/disk', (req, res) => {
-    si.fsSize().then(disk => {
-        var used = bytesToSize(disk[0].used);
-        var size = bytesToSize(disk[0].size);
-        res.json({
-            used:used,
-            size:size
-        })
+  si.fsSize().then(disk => {
+    var used = bytesToSize(disk[0].used)
+    var size = bytesToSize(disk[0].size)
+    res.json({
+      used: used,
+      size: size
     })
+  })
 
-    var bytesToSize = (bytes) => {
-        var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-        if (bytes == 0) return '0 Byte';
-        var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
-        return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
-    };
+  var bytesToSize = (bytes) => {
+    var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB']
+    if (bytes == 0) return '0 Byte'
+    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)))
+    return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i]
+  }
 })
 
-
-app.get('/cpu', (req,res) => {
-    si.currentLoad().then(data => {
-        var load = data.cpus[0].load;
-        res.json({
-            'cpu-load':load
-        })
+app.get('/cpu', (req, res) => {
+  si.currentLoad().then(data => {
+    var load = data.cpus[0].load
+    res.json({
+      'cpu-load': load
     })
+  })
 })
 
-var port = process.env.PORT || 8080;
-app.listen(port);
+var port = process.env.PORT || 8080
+app.listen(port)
--- public/main.js
+++ public/main.js
@@ -1,93 +1,91 @@
-var tempResult = document.getElementById('temp-result');
-var psResult = document.getElementById('ps-result');
-var netResult = document.querySelectorAll('#net-result > p > span');
+var tempResult = document.getElementById('temp-result')
+var psResult = document.getElementById('ps-result')
+var netResult = document.querySelectorAll('#net-result > p > span')
 var osResult = document.querySelectorAll('#os-result > p > span')
-var diskResult = document.querySelectorAll('.disk-usage ~ p > span');
+var diskResult = document.querySelectorAll('.disk-usage ~ p > span')
 
-var url = window.location.origin;
+var url = window.location.origin
 setInterval(() => {
-    fetch(url + '/temp')
+  fetch(url + '/temp')
         .then(data => data.json())
         .then(json => {
-            tempResult.innerText = json.temperature + '°C';
-        })
-}, 2000)
-
-
-setInterval(() => {
-    fetch(url + '/ps')
-        .then(data => data.json())
-        .then(json => {
-            var result = "<tr><th>PID</th><th>Name</th><th>CPU %</th></tr>";
-            json.forEach((item) => {
-                result += `<tr><td>${item.pid}</td><td>${item.name}</td><td>${item.pcpu}</td><tr>`;
-            })
-
-            psResult.innerHTML = result;
+          tempResult.innerText = json.temperature + '°C'
         })
 }, 2000)
 
 setInterval(() => {
-    fetch(url + '/net')
+  fetch(url + '/ps')
         .then(data => data.json())
         .then(json => {
-            netResult[0].innerText = json.rx_sec.toFixed(1);
-            netResult[1].innerText = json.tx_sec.toFixed(1);
-        })
-}, 2000)
+          var result = '<tr><th>PID</th><th>Name</th><th>CPU %</th></tr>'
+          json.forEach((item) => {
+            result += `<tr><td>${item.pid}</td><td>${item.name}</td><td>${item.pcpu}</td><tr>`
+          })
 
-
-setInterval(() => {
-    fetch(url + '/os')
-        .then(data => data.json())
-        .then(json => {
-            osResult[0].innerText = json.platform;
-            osResult[1].innerText = json.distro;
-            osResult[2].innerText = json.arch;          
-            osResult[3].innerText = json.hostname; 
+          psResult.innerHTML = result
         })
 }, 2000)
 
 setInterval(() => {
-    fetch(url + '/disk')
+  fetch(url + '/net')
         .then(data => data.json())
         .then(json => {
-            diskResult[0].innerText = 'Used: ' + json.used;
-            diskResult[1].innerText = 'Size: ' + json.size;
+          netResult[0].innerText = json.rx_sec.toFixed(1)
+          netResult[1].innerText = json.tx_sec.toFixed(1)
         })
 }, 2000)
 
-var cpuHistory = [];
 setInterval(() => {
-    fetch(url + '/cpu')
+  fetch(url + '/os')
         .then(data => data.json())
         .then(json => {
-            var result = Math.round(json['cpu-load']);
-            cpuHistory.push(result);
-            var total = cpuHistory.reduce((x,y) => {
-                return x + y; 
-            })
-            var cpuAvg = document.getElementById('cpu-avg');
-            var average = Math.round(total / 6);
-            console.log(average);
-            cpuAvg.innerText = 'Average: ' + average + '%';
+          osResult[0].innerText = json.platform
+          osResult[1].innerText = json.distro
+          osResult[2].innerText = json.arch
+          osResult[3].innerText = json.hostname
+        })
+}, 2000)
 
-            if (cpuHistory.length > 6) {
-                cpuHistory.shift();
+setInterval(() => {
+  fetch(url + '/disk')
+        .then(data => data.json())
+        .then(json => {
+          diskResult[0].innerText = 'Used: ' + json.used
+          diskResult[1].innerText = 'Size: ' + json.size
+        })
+}, 2000)
+
+var cpuHistory = []
+setInterval(() => {
+  fetch(url + '/cpu')
+        .then(data => data.json())
+        .then(json => {
+          var result = Math.round(json['cpu-load'])
+          cpuHistory.push(result)
+          var total = cpuHistory.reduce((x, y) => {
+            return x + y
+          })
+          var cpuAvg = document.getElementById('cpu-avg')
+          var average = Math.round(total / 6)
+          console.log(average)
+          cpuAvg.innerText = 'Average: ' + average + '%'
+
+          if (cpuHistory.length > 6) {
+            cpuHistory.shift()
+          }
+          var data = {
+            series: [cpuHistory]
+          }
+
+          var options = {
+            axisY: {
+              type: Chartist.FixedScaleAxis,
+              ticks: [0, 20, 40, 60, 80, 100],
+              high: 100,
+              low: 0
             }
-            var data = {
-                series: [cpuHistory]
-            };
+          }
 
-            var options = {
-                axisY: {
-                    type: Chartist.FixedScaleAxis,
-                    ticks: [0, 20, 40, 60, 80, 100],
-                    high: 100,
-                    low: 0
-                },
-            }
-
-            new Chartist.Line('.ct-chart', data, options);
+          new Chartist.Line('.ct-chart', data, options)
         })
 }, 1000)
